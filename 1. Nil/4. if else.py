a3e35b3e5e889733161d43ca09bf09c072840a74
adarshrise <adarshrise20@gmail.com>
2019-03-16 23:17:17 +0530

    added if else file

---
+++ 1. Nil/4. if else.py
@@ -0,0 +1,22 @@
+# Making choices with If else
+x=4
+if x==4:
+    print(" it was equal ")
+else:
+    print(" it was not equal ")
+
+if (x>4):
+    print(" x is  more than 4 ")
+else:
+    print(" x is not more than 4 " )
+
+if(x<4):
+    print(" x is lower ")
+
+else:
+    print(" x is greater ")
+
+if (x!=4):
+    print(" x is not 4")
+else:
+    print(" x is 4 ")
